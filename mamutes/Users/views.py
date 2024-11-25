47b88ef47b1730027a0cf0824d3ce293a7c49dde
isaqzin <isaquereserva@gmail.com>
2024-11-24 22:36:49 -0300

    CadastroTelaEIntegracao
    Co-authored-by: ludmilaaysha <231026750@aluno.unb.br>

--- mamutes/Users/views.py
+++ mamutes/Users/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth import authenticate, login as login_django
 from .models import * 
 from .forms import *
 
 def login (request):
     if request.method ==  'GET':
-        return render (request, '/login.html')
+        return render (request, 'login.html')
     else:
         username = request.POST.get('username')
         senha = request.POST.get('senha')
@@ -20,4 +21,13 @@ def login (request):
             
         else:
             return HttpResponse("Credenciais inválidas.")
+
+        
+def isSuperUser(user):
+    return user.is_superuser
+    
+    
+@user_passes_test(isSuperUser) 
+def cadastro(request):
+     return render(request, 'cadastro.html')
 
