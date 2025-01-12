55d331941105550c07991c88fbde1663c60113ea
Ludmila Nunes <ludmila.aysha@gmail.com>
2025-01-12 04:28:56 -0300

    Responsividade sidebar e header

--- mamutes/static/scripts/Top.js
+++ mamutes/static/scripts/Top.js
@@ -3,12 +3,35 @@ const popup = document.querySelector('.Popup');
 const notificationButton = document.querySelector('.ButtonNotif');
 const notificationPopup = document.querySelector('.Notifications');
 
-
 button.addEventListener('click', (event) => {
     // Evita que o clique na imagem feche o popup imediatamente
     event.stopPropagation();
 
-    // Alterna a classe 'show' para mostrar/ocultar o popup
+    if (notificationPopup.classList.contains('is-visible')) {
+        notificationPopup.classList.remove('is-visible');
+        notificationButton.classList.remove('active');
+    }
+
+    // Alterna a classe 'active' para mudar o estilo da imagem
+    button.classList.toggle('active');
+
+    // // Alterna a classe 'show' para mostrar/ocultar o popup
+    popup.classList.toggle('show');
+});
+
+notificationButton.addEventListener('click', (event) => {
+    // Evita que o clique na imagem feche o popup imediatamente
+    event.stopPropagation();
+
+    if (popup.classList.contains('show')) {
+        popup.classList.remove('show');
+        button.classList.remove('active');
+    }
+
+    // Alterna a classe 'active' para mudar o estilo da imagem
+    // button.classList.toggle('active');
+
+    // // Alterna a classe 'show' para mostrar/ocultar o popup
     popup.classList.toggle('show');
 });
 
@@ -17,6 +40,7 @@ document.addEventListener('click', (event) => {
     // Verifica se o clique foi fora da imagem e do popup
     if (!button.contains(event.target) && !popup.contains(event.target)) {
         popup.classList.remove('show'); // Esconde o popup
+        button.classList.remove('active'); // Remove a classe 'active' da imagem
     }
 });
 
@@ -26,6 +50,9 @@ notificationButton.addEventListener('click', (event) => {
 
     // Alterna a classe 'is-visible' para mostrar/ocultar as notificações
     notificationPopup.classList.toggle('is-visible');
+
+    // Alterna a classe 'active' para mudar o estilo do botão de notificação
+    notificationButton.classList.toggle('active');
 });
 
 // Adiciona evento de clique no documento para esconder as notificações quando clicar fora
@@ -33,7 +60,6 @@ document.addEventListener('click', (event) => {
     // Verifica se o clique foi fora do botão de notificação e do popup
     if (!notificationButton.contains(event.target) && !notificationPopup.contains(event.target)) {
         notificationPopup.classList.remove('is-visible'); // Esconde o popup
+        notificationButton.classList.remove('active'); // Remove a classe 'active' do botão
     }
 });
-
-
