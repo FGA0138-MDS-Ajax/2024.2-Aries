10619f9790d5230cbefef8536e9e187c9b1557c6
CaioDuart3 <caioduarte330@gmail.com>
2025-01-31 14:59:52 -0300

    lógica no filtro da home

--- mamutes/members/static/scripts/modal.js
+++ mamutes/members/static/scripts/modal.js
@@ -1,25 +1,26 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const responsiblesField = document.getElementById('responsibles');
-//     const plusMembers = document.querySelectorAll('.plus-members');
+document.addEventListener('DOMContentLoaded', () => {
+    const responsiblesField = document.getElementById('responsibles');
+    const plusMembers = document.querySelectorAll('.plus-members');
     
 
-//     plusMembers.forEach(member => {
-//         member.addEventListener('click', () => {
-//             const memberId = member.getAttribute('data-id');
+    plusMembers.forEach(member => {
+        member.addEventListener('click', () => {
+            const memberId = member.getAttribute('data-id');
 
             
-//             // Obtém o valor atual do campo
-//             let currentValues = responsiblesField.value ? responsiblesField.value.split(',') : [];
+            // Obtém o valor atual do campo
+            let currentValues = responsiblesField.value ? responsiblesField.value.split(',') : [];
             
-//             // Adiciona o novo ID, se ainda não estiver na lista
-//             if (!currentValues.includes(memberId)) {
-//                 currentValues.push(memberId);
-//             }
-//             // Atualiza o campo com os novos valores
-//             responsiblesField.value = currentValues.join(',');
+            // Adiciona o novo ID, se ainda não estiver na lista
+            if (!currentValues.includes(memberId)) {
+                currentValues.push(memberId);
+            }
+            // Atualiza o campo com os novos valores
+            responsiblesField.value = currentValues.join(',');
 
-//             const personTeam = member.closest('.person-team'); // Encontra o elemento pai .person-team
-//             personTeam.classList.toggle('selected-person-team');
-//         });
-//     });
-// });  
+            const personTeam = member.closest('.person-team'); // Encontra o elemento pai .person-team
+            personTeam.classList.toggle('selected-person-team');
+        });
+    });
+});  
+
--- mamutes/members/static/scripts/scriptMembers.js
+++ mamutes/members/static/scripts/scriptMembers.js
@@ -1,29 +1,25 @@
-
-// função para ao clicar, os filtros mudarem de cor
+// Função para ao clicar, os filtros mudarem de cor
 function selectWord(clickedWord) {
-
     const tabContent = document.querySelectorAll('.tabContent');
     tabContent.forEach(word => word.classList.remove('active'));
-    
     clickedWord.classList.add('active');
 }
 
 document.addEventListener("click", (e) => {
     const event = e.target;
-    const announcement = document.querySelector(".announcement");
-    const eventful = document.querySelector(".event");
-    
-    if (event.id == "announcement"){
-        eventful.style.display = "none";
-        announcement.style.display = "flex";
-    }
-    if (event.id == "event"){
-        announcement.style.display = "none";
-        eventful.style.display = "flex";
-    }
-    if (event.id == "all"){
-        announcement.style.display = "flex";
-        eventful.style.display = "flex";
-    }
+    const announcements = document.querySelectorAll(".announcement");
+    const events = document.querySelectorAll(".event");
 
-})
+    if (event.id === "announcement") {
+        announcements.forEach(announcement => announcement.style.display = "flex");
+        events.forEach(eventful => eventful.style.display = "none");
+    } 
+    else if (event.id === "event") {
+        announcements.forEach(announcement => announcement.style.display = "none");
+        events.forEach(eventful => eventful.style.display = "flex");
+    } 
+    else if (event.id === "all") {
+        announcements.forEach(announcement => announcement.style.display = "flex");
+        events.forEach(eventful => eventful.style.display = "flex");
+    }
+});
