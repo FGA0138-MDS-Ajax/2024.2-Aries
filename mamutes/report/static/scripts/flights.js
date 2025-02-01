37d434978be20d29fe05f62abd73ce3e76e1b885
Mayara Marques <231035731@aluno.unb.br>
2025-02-01 20:34:11 -0300

    feat: nova versão modal newFlight
    Co-authored-by: ludmilaaysha <231026750@aluno.unb.br>

--- mamutes/report/static/scripts/flights.js
+++ mamutes/report/static/scripts/flights.js
@@ -32,7 +32,8 @@ const submitCancelBotao = document.querySelector(".submitCancelBotao");
 //         alert('OI MUNDO')
 //     }
 // })
-
+ 
+// abrir modal novo flight
 document.addEventListener("DOMContentLoaded", function () {
 
     const openModalBtn = document.getElementById("openModalBtn");
@@ -53,3 +54,91 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+// rating de voo
+document.addEventListener("DOMContentLoaded", function () {
+    const stars = document.querySelectorAll(".star");
+    const ratingText = document.getElementById("ratingText");
+
+    const ratings = ["Ruim", "Regular", "Bom", "Muito Bom", "Ótimo"];
+
+    stars.forEach((star, index) => {
+        star.addEventListener("click", function () {
+            let selectedValue = this.getAttribute("data-value");
+
+            stars.forEach((s, i) => {
+                if (i < selectedValue) {
+                    s.classList.add("selected");
+                } else {
+                    s.classList.remove("selected");
+                }
+            });
+
+            ratingText.textContent = ratings[selectedValue - 1];
+        });
+    });
+});
+
+// mostra div adcional se checkbox marcado
+document.addEventListener("DOMContentLoaded", function () {
+    const checkbox = document.getElementById("accidentCheckbox");
+    const extraContent = document.getElementById("additionalInputs");
+
+    checkbox.addEventListener("change", function () {
+        if (this.checked) {
+            extraContent.classList.remove("accidentBox");
+        } else {
+            extraContent.classList.add("accidentBox");
+        }
+    });
+});
+
+// estilização para dropdown
+function showOptions(e) {
+    let divOptions = document.getElementById("divOptions");
+    if (divOptions.style.display == "none" || divOptions.style.display == "") {
+        divOptions.style.display = "inline-block";
+    } else {
+        divOptions.style.display = "none";
+    }
+}
+function clickMe(e) {
+    console.log('click me');
+    e.stopPropagation();
+}
+function hideOptions(e) {
+    let divOptions = document.getElementById("divOptions");
+
+    if (divOptions.contains(e.target)) {
+        divOptions.style.display = "inline-block";
+    } else {
+        divOptions.style.display = "none";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    let checkbox = document.querySelectorAll("#divOptions input");
+    let inputCheckbox = document.getElementById("inputCheckbox");
+
+    for (let i = 0; i < checkbox.length; i++) {
+        checkbox[i].addEventListener("change", (e) => {
+            if (e.target.checked == true) {
+                if (inputCheckbox.value == "") {
+                    inputCheckbox.value = checkbox[i].value;
+                } else {
+                    inputCheckbox.value += `,${checkbox[i].value}`;
+                }
+            } else {
+                let values = inputCheckbox.value.split(",");
+
+                for (let r = 0; r < values.length; r++) {
+                    if (values[r] == e.target.value) {
+                        values.splice(r, 1);
+                    }
+                }
+                inputCheckbox.value = values;
+            }
+        });
+    }
+});
+
