f5dd662dd4c20b47af78a53c1de244625e746ef1
CaioDuart3 <caioduarte330@gmail.com>
2025-02-01 19:45:26 -0300

    correção de bug de setar responsáveis pela tarefa

--- mamutes/members/static/scripts/setResponsibles.js
+++ mamutes/members/static/scripts/setResponsibles.js
@@ -1,26 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const responsiblesField = document.getElementById('responsibles');
     const plusMembers = document.querySelectorAll('.plus-members');
-    
 
     plusMembers.forEach(member => {
         member.addEventListener('click', () => {
             const memberId = member.getAttribute('data-id');
+            if (!memberId) {
+                return;
+            }
 
-            
+
+            // Encontra o input #responsibles mais próximo do clique
+            const responsiblesField = member.closest('.form')?.querySelector('#responsibles');
+
+            if (!responsiblesField) {
+                return;
+            }
+
             // Obtém o valor atual do campo
             let currentValues = responsiblesField.value ? responsiblesField.value.split(',') : [];
-            
-            // Adiciona o novo ID, se ainda não estiver na lista
+
+            // Adiciona o novo ID se ainda não estiver na lista
             if (!currentValues.includes(memberId)) {
                 currentValues.push(memberId);
             }
-            // Atualiza o campo com os novos valores
+
+            // Atualresponsibles-containeriza o campo com os novos valores
             responsiblesField.value = currentValues.join(',');
 
             const personTeam = member.closest('.person-team'); // Encontra o elemento pai .person-team
-            personTeam.classList.toggle('selected-person-team');
+            if (personTeam) {
+                personTeam.classList.toggle('selected-person-team');
+            }
         });
     });
-});  
-
+});
