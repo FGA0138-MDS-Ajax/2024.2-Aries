577991d0ba170247e5cf0112969a7dfe65aeccb7
CaioDuart3 <caioduarte330@gmail.com>
2025-02-02 18:44:26 -0300

    redirecionamento para o p.s na index quando ele está aberto

--- mamutes/guest/views.py
+++ mamutes/guest/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect
 from .models import AdmissionState
 
 def index(request):
-    return render(request, 'index.html')
+    state = AdmissionState.objects.first()
+    if not state:
+        state = AdmissionState.objects.create(is_open=True)
+    return render(request, 'index.html', {'state':state})
 
 def competition(request):
     return render(request, 'comp.html')
